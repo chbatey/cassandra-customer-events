72f1d861afe05fef158a8e412e972164b623cf19
Christopher Batey <christopher.batey@gmail.com>
2014-11-24 18:59:34 +0000

    removing mapping annotations from master

--- src/main/java/info/batey/eventstore/CustomerEvent.java
+++ src/main/java/info/batey/eventstore/CustomerEvent.java
@@ -1,20 +1,11 @@
 package info.batey.eventstore;
 
-import com.datastax.driver.mapping.annotations.ClusteringColumn;
-import com.datastax.driver.mapping.annotations.Column;
-import com.datastax.driver.mapping.annotations.PartitionKey;
-import com.datastax.driver.mapping.annotations.Table;
-
-import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
-@Table(keyspace = "customers", name = "customer_events")
 public class CustomerEvent {
-    @PartitionKey
     private String customerId;
 
-    @ClusteringColumn()
     private UUID time;
 
     private String staffId;
@@ -38,47 +29,23 @@ public class CustomerEvent {
         return customerId;
     }
 
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
+    public UUID getTime() {
+        return time;
     }
 
     public String getStaffId() {
         return staffId;
     }
 
-    public void setStaffId(String staffId) {
-        this.staffId = staffId;
-    }
-
-    public UUID getTime() {
-        return time;
-    }
-
-    public void setTime(UUID time) {
-        this.time = time;
-    }
-
     public String getStoreType() {
         return storeType;
-    }
-
-    public void setStoreType(String storeType) {
-        this.storeType = storeType;
     }
 
     public String getEventType() {
         return eventType;
     }
 
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
     public Map<String, String> getTags() {
         return tags;
-    }
-
-    public void setTags(Map<String, String> tags) {
-        this.tags = tags;
     }
 }
