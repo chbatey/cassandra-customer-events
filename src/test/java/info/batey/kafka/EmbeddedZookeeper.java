34f626aed7e5da5ee03d46ae95e938f38ec00853
Christopher Batey <christopher.batey@gmail.com>
2015-01-29 14:39:22 +0000

    wip

--- src/test/java/info/batey/kafka/EmbeddedZookeeper.java
+++ src/test/java/info/batey/kafka/EmbeddedZookeeper.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class EmbeddedZookeeper {
-    private int port = -1;
+    private int port = 2181;
     private int tickTime = 500;
 
     private ServerCnxnFactory factory;
@@ -17,35 +17,20 @@ public class EmbeddedZookeeper {
     private File logDir;
 
     public EmbeddedZookeeper() {
-        this(-1);
+
     }
 
-    public EmbeddedZookeeper(int port) {
-        this(port, 500);
-    }
-
-    public EmbeddedZookeeper(int port, int tickTime) {
-        this.port = resolvePort(port);
-        this.tickTime = tickTime;
-    }
-
-    private int resolvePort(int port) {
-        if (port == -1) {
-            return TestUtils.getAvailablePort();
-        }
-        return port;
-    }
 
     public void startup() throws IOException{
-        if (this.port == -1) {
-            this.port = TestUtils.getAvailablePort();
-        }
-        this.factory = NIOServerCnxnFactory.createFactory(new InetSocketAddress("localhost", port), 1024);
+
         this.snapshotDir = TestUtils.constructTempDir("embeeded-zk/snapshot");
         this.logDir = TestUtils.constructTempDir("embeeded-zk/log");
 
         try {
-            factory.startup(new ZooKeeperServer(snapshotDir, logDir, tickTime));
+            ZooKeeperServer zkServer = new ZooKeeperServer(snapshotDir, logDir, tickTime);
+            this.factory = NIOServerCnxnFactory.createFactory();
+            this.factory.configure(new InetSocketAddress("localhost", port), 16);
+            factory.startup(zkServer);
         } catch (InterruptedException e) {
             throw new IOException(e);
         }
@@ -64,33 +49,5 @@ public class EmbeddedZookeeper {
         } catch (FileNotFoundException e) {
             // ignore
         }
-    }
-
-    public String getConnection() {
-        return "localhost:" + port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public void setTickTime(int tickTime) {
-        this.tickTime = tickTime;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public int getTickTime() {
-        return tickTime;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("EmbeddedZookeeper{");
-        sb.append("connection=").append(getConnection());
-        sb.append('}');
-        return sb.toString();
     }
 }
--- src/test/java/info/batey/kafka/Sandbox.java
+++ src/test/java/info/batey/kafka/Sandbox.java
@@ -2,8 +2,7 @@ package info.batey.kafka;
 
 public class Sandbox {
     public static void main(String[] args) throws Exception {
-
-        EmbeddedZookeeper ex = new EmbeddedZookeeper(12000);
+        EmbeddedZookeeper ex = new EmbeddedZookeeper();
         ex.startup();
     }
 }
